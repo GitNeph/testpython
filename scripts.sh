6a12eaf735b9809153975117a05e3345e974ca44
KlutzyBubbles <KlutzyBubbles@users.noreply.github.com>
2021-11-23 15:39:33 +1100

    Create scripts.sh

---
+++ scripts.sh
@@ -0,0 +1,14 @@
+python -m build
+python -m pip install ./dist/testpython-0.0.2-py3-none-any.whl --force-reinstall
+
+tp-one
+tp-two
+tp-three
+tp-four
+
+# Maybe with python -m
+
+pyhton -m tp-one
+pyhton -m tp-two
+pyhton -m tp-three
+pyhton -m tp-four
